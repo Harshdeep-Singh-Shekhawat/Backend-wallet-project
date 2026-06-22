8983632f1d8f53465976ceab298e972a5f926647
Harshdeep-Singh-Shekhawat <harshdeep.shekhawat@iitgn.ac.in>
2026-06-22 21:02:01 +0530

    Fix Google button styling and tsconfig deprecation warning

--- frontend/src/components/AuthScreen.tsx
+++ frontend/src/components/AuthScreen.tsx
@@ -128,7 +128,7 @@ export default function AuthScreen({ onSuccess }: AuthScreenProps) {
 
         <button 
           onClick={() => window.location.href = `${API_URL}/api/auth/google`} 
-          className={styles.btnSecondary}
+          className={styles.btnGoogle}
         >
           <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
             <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
